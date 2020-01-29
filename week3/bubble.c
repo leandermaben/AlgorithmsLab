79193107d28051d3617811d7ac0c8900df602d01
leandermaben <leander.maben@gmail.com>
2020-01-29 23:22:55 +0530

    Added Graph

--- week3/bubble.c
+++ week3/bubble.c
@@ -1,10 +1,13 @@
 #include<stdio.h>
 #include<stdlib.h>
+int opcount=0;
 void bubble(int* a,int n){
 	int i,j,temp;
+	opcount=0;
 	for(i=0;i<n-1;i++){
 		for(j=0;j<n-1-i;j++){
 			if(a[j]>a[j+1]){
+				opcount++;
 				temp=a[j];
 				a[j]=a[j+1];
 				a[j+1]=temp;
@@ -14,15 +17,23 @@ void bubble(int* a,int n){
 }
 int main(){
 	int n,i;
-	printf("\nEnter number of elements.");
-	scanf("%d",&n);
-	int*a=malloc(n*sizeof(int));
-	for(i=0;i<n;i++){
+	int a[100];
+	//printf("\nEnter number of elements.");
+	//scanf("%d",&n);
+	//int*a=malloc(n*sizeof(int));
+	/*for(i=0;i<n;i++){
 		scanf("%d",&a[i]);
+	}*/
+	for(i=1;i<21;i++){
+		for(int j=0;j<i;j++){
+			a[j]=i-j;
+		}
+		bubble(a,i);
+		printf("\nopcount: %d \tn: %d",opcount,i);
 	}
-	bubble(a,n);
+	
 	printf("\n");
-	for(i=0;i<n;i++){
+	/*for(i=0;i<n;i++){
 		printf("\t%d",a[i]);
-	}
+	}*/
 }
