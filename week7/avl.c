43cf68cadfc4bef437fd295c490d35d184258494
leandermaben <leander.maben@gmail.com>
2020-02-23 19:36:47 +0530

    Made the insertion and update iterartive

--- week7/avl.c
+++ week7/avl.c
@@ -6,6 +6,10 @@ typedef struct node{
 	struct node* right;
 	int height;
 }node;
+typedef struct post{
+	node* cur;
+	int flag;
+}post;
 typedef struct stack{
 	node* a[100];
 	int tos;
@@ -28,6 +32,21 @@ int balance_factor(node* ancs){
 void leftRotate(node* cur,node** root,node* par){
 	node* temp=cur->right->left;
 	node* temp2=cur->right;
+	int left,right;
+	if(cur->left)
+		left=cur->left->height;
+	else 
+		left=0;
+	if(cur->right->left)
+		right=cur->right->left->height;
+	else
+		right=0;
+	cur->height=1+max(left,right);
+	if(cur->right->right)
+		right=cur->right->right->height;
+	else
+		right=0;
+	cur->right->height=1+max(cur->height,right);
 	cur->right->left=cur;
 	cur->right=temp;
 	if(*root==cur)
@@ -40,6 +59,21 @@ void leftRotate(node* cur,node** root,node* par){
 void rightRotate(node* cur,node** root,node* par){
 	node* temp=cur->left->right;
 	node* temp2=cur->left;
+	int right,left;
+	if(cur->right)
+		right=cur->right->height;
+	else 
+		right=0;
+	if(cur->left->right)
+		left=cur->left->right->height;
+	else
+		left=0;
+	cur->height=1+max(left,right);
+	if(cur->left->left)
+		left=cur->left->left->height;
+	else
+		left=0;
+	cur->left->height=1+max(cur->height,left);
 	cur->left->right=cur;
 	cur->left=temp;
 	if(*root==cur)
@@ -92,24 +126,53 @@ void update(node* root){
 		right=root->right->height;
 	root->height=1+max(left,right);
 }
-void insert(node** root,node** point,int data,stack* s){
+void update1(node* root,stack* s){
+	node* temp;
+	int right,left;
+	while(s->tos!=-1){
+		temp=s->a[(s->tos)--];
+		if(temp->left)
+			left=temp->left->height;
+		else
+			left=0;
+		if(temp->right)
+			right=temp->right->height;
+		else
+			right=0;
+		temp->height=1+max(left,right);
+		printf("\n%d-%d",temp->data,temp->height);
+	}
+}
+void insert(node** root,int data){
+	node* temp=malloc(sizeof(node));
+	node *trav=NULL,*prev=NULL;
+	stack s,t,t2;
+	s.tos=-1;
+	temp->data=data;
+	temp->left=NULL;
+	temp->right=NULL;
+	temp->height=1;
 	if(!(*root)){
-		*root=malloc(sizeof(node));
-		(*root)->data=data;
-		(*root)->left=NULL;
-		(*root)->right=NULL;
-		update(*point);
-		if(s->tos!=-1){
-			balance(data,point,s);
-		}
-		return;
+		*root=temp;
 	}else{
-		s->a[++(s->tos)]=*root;
-		if(data<(*root)->data){
-			insert(&((*root)->left),point,data,s);
-		}else{
-			insert(&((*root)->right),point,data,s);
+		trav=*root;
+		for(;trav;){
+			s.a[++(s.tos)]=trav;
+			prev=trav;
+			if(data<trav->data)
+				trav=trav->left;
+			else
+				trav=trav->right;
 		}
+		if(data<prev->data)
+			prev->left=temp;
+		else
+			prev->right=temp;
+		t=s;
+		t2=s;
+		t2.a[++(t2.tos)]=temp;
+		update1(*root,&t);
+		balance(data,root,&s);
 	}
 }
 void inorder(node* root){
@@ -135,14 +198,12 @@ void postorder(node* root){
 }
 int main(){
 	node* root=NULL;
-	stack s;
 	int n,data,i;
 	printf("\nEnter number of nodes");
 	scanf("%d",&n);
 	for(i=0;i<n;i++){
 		scanf("%d",&data);
-		s.tos=-1;
-		insert(&root,&root,data,&s);
+		insert(&root,data);
 	}
 	printf("\nPre order\n");
 	preorder(root);
