0a06d3a78ec9a59d2d3e8568b80e21b3a1a86013
leandermaben <leander.maben@gmail.com>
2020-02-24 23:12:09 +0530

    Fixed AVL Code

--- week7/avl.c
+++ week7/avl.c
@@ -13,36 +13,20 @@ typedef struct stack{
 int max(int a,int b){
 	return a>b?a:b;
 }
+int height(node* x){
+	if(!x)
+		return 0;
+	else 
+		return x->height;
+}
 int balance_factor(node* ancs){
-	int left,right;
-	if(ancs->left==NULL)
-		left=0;
-	else
-		left=ancs->left->height;
-	if(ancs->right==NULL)
-		right=0;
-	else
-		right=ancs->right->height;
-	return left-right;
+	return height(ancs->left)-height(ancs->right);
 }
 void leftRotate(node* cur,node** root,node* par){
 	node* temp=cur->right->left;
 	node* temp2=cur->right;
-	int left,right;
-	if(cur->left)
-		left=cur->left->height;
-	else 
-		left=0;
-	if(cur->right->left)
-		right=cur->right->left->height;
-	else
-		right=0;
-	cur->height=1+max(left,right);
-	if(cur->right->right)
-		right=cur->right->right->height;
-	else
-		right=0;
-	cur->right->height=1+max(cur->height,right);
+	cur->height=1+max(height(cur->left),height(temp));
+	cur->right->height=1+max(cur->height,height(temp2->right));
 	cur->right->left=cur;
 	cur->right=temp;
 	if(*root==cur)
@@ -55,21 +39,8 @@ void leftRotate(node* cur,node** root,node* par){
 void rightRotate(node* cur,node** root,node* par){
 	node* temp=cur->left->right;
 	node* temp2=cur->left;
-	int right,left;
-	if(cur->right)
-		right=cur->right->height;
-	else 
-		right=0;
-	if(cur->left->right)
-		left=cur->left->right->height;
-	else
-		left=0;
-	cur->height=1+max(left,right);
-	if(cur->left->left)
-		left=cur->left->left->height;
-	else
-		left=0;
-	cur->left->height=1+max(cur->height,left);
+	cur->height=1+max(height(cur->right),height(temp));
+	cur->right->height=1+max(cur->height,height(temp2->left));
 	cur->left->right=cur;
 	cur->left=temp;
 	if(*root==cur)
@@ -79,53 +50,32 @@ void rightRotate(node* cur,node** root,node* par){
 	else
 		par->right=temp2;
 }
-void balance(int key,node** root,stack* s){
+void balance(int key,node** root,node* cur,node* par){
 	int fac;
-	node *temp1=NULL,*par;
-	do{
-		temp1=s->a[(s->tos)--];
-		fac=balance_factor(temp1);
-	}while(fac<2 && fac>-2&&s->tos!=-1);
-	if(s->tos!=-1)
-		par=s->a[(s->tos)--];
-	else
-		par=NULL;
+	fac=balance_factor(cur);
 	if(fac==0)
 		return;
-	else if(fac>1&&key<temp1->left->data){
-		rightRotate(temp1,root,par);
+	else if(fac>1&&key<cur->left->data){
+		rightRotate(cur,root,par);
 	}
-	else if(fac>1&&key>temp1->left->data){
-		leftRotate(temp1->left,root,temp1);
-		rightRotate(temp1,root,par);
-	}else if(fac<-1&&key>temp1->right->data){
-		leftRotate(temp1,root,par);
+	else if(fac>1&&key>cur->left->data){
+		leftRotate(cur->left,root,cur);
+		rightRotate(cur,root,par);
+	}else if(fac<-1&&key>cur->right->data){
+		leftRotate(cur,root,par);
 	}
-	else if(fac>1&&key<temp1->right->data){
-		rightRotate(temp1->right,root,temp1);
-		leftRotate(temp1,root,par);
+	else if(fac>1&&key<cur->right->data){
+		rightRotate(cur->right,root,cur);
+		leftRotate(cur,root,par);
 	}
 }
-void update(node* root,stack* s){
-	node* temp;
-	int right,left;
-	while(s->tos!=-1){
-		temp=s->a[(s->tos)--];
-		if(temp->left)
-			left=temp->left->height;
-		else
-			left=0;
-		if(temp->right)
-			right=temp->right->height;
-		else
-			right=0;
-		temp->height=1+max(left,right);
-	}
+void update(node* cur){
+	cur->height=1+max(height(cur->left),height(cur->right));
 }
 void insert(node** root,int data){
 	node* temp=malloc(sizeof(node));
 	node *trav=NULL,*prev=NULL;
-	stack s,t,t2;
+	stack s;
 	s.tos=-1;
 	temp->data=data;
 	temp->left=NULL;
@@ -147,11 +97,15 @@ void insert(node** root,int data){
 			prev->left=temp;
 		else
 			prev->right=temp;
-		t=s;
-		t2=s;
-		t2.a[++(t2.tos)]=temp;
-		update(*root,&t);
-		balance(data,root,&s);
+		node* par=s.a[(s.tos)--];
+		while(s.tos!=-1){
+			trav=par;
+			par=s.a[(s.tos)--];
+			update(trav);
+			balance(data,root,trav,par);
+		}
+		update(par);
+		balance(data,root,par,NULL);
 	}
 }
 void inorder(node* root){
